c8d6403cb706438c39eb1983e0275bca6073802b
Xiang Li <x.li@imperial.ac.uk>
2017-09-25 17:04:57 +0100

    this folder is made for the R traning short course

---
+++ R_short_course/questions.R
@@ -0,0 +1,18 @@
+##--------------------- Day 1 ---------------------## 
+
+
+##--------------------- Day 2 ---------------------## 
+vignette("chapter1", package = "jrShiny")
+jrShiny::create_project()
+# [1] FALSE
+# Warning messages:
+#   1: In file.copy(file.path(path, paste0(chapter, ".Rmd")), dest) :
+#   problem copying /home/xli17/R/x86_64-pc-linux-gnu-library/3.4/jrShiny/doc/chapter5.Rmd to original/chapter5.Rmd: No such file or directory
+# 2: In file.copy(file.path(path, paste0(chapter, ".Rmd")), dest) :
+#   problem copying /home/xli17/R/x86_64-pc-linux-gnu-library/3.4/jrShiny/doc/chapter5.Rmd to exercises/chapter5.Rmd: No such file or directory
+
+Numbering headers - not manual but automatic 
+- eg. 
+start from the first level header - 1
+follow the first level header and reach second level - 1.1
+##--------------------- Day 3 ---------------------## 
