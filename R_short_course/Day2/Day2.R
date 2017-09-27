0cecfaa2f3cf1e1b64219baada30ed6982e3c67f
Xiang Li <x.li@imperial.ac.uk>
2017-09-27 11:53:09 +0100

    nothing special

--- R_short_course/Day2/Day2.R
+++ R_short_course/Day2/Day2.R
@@ -1,1 +1,14 @@
-#000
+drat::addRepo("rcourses")
+install.packages("nclRshiny", dependencies = TRUE)
+library("nclRshiny")
+
+vignette("slides1", package = "nclRshiny")
+
+vignette("chapter1", package = "nclRshiny")
+
+
+
+vignette("chapter1", package = "nclRshiny")
+nclRshiny::create_project()
+
+
