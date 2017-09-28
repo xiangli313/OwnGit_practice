56c9d70160717c69d0f82a4ca7918f009d67115c
Xiang Li <x.li@imperial.ac.uk>
2017-09-28 09:12:25 +0100

    day2

--- R_short_course/Day2/Day2.R
+++ R_short_course/Day2/Day2.R
@@ -2,13 +2,17 @@ drat::addRepo("rcourses")
 install.packages("nclRshiny", dependencies = TRUE)
 library("nclRshiny")
 
-vignette("slides1", package = "nclRshiny")
+vignette("slides4", package = "jrShiny")
 
-vignette("chapter1", package = "nclRshiny")
+vignette("chapter2", package = "nclRshiny")
 
 
 
 vignette("chapter1", package = "nclRshiny")
 nclRshiny::create_project()
+
+
+#### chapter 2
+vignette("htmlwidgets", package="nclRshiny")
 
 
